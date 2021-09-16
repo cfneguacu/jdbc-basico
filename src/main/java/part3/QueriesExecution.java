7b2996acea6ee4ad2d0ec65457f6e4fce14a75c3
Claudio Francisco das Neves <59265698+claudioneves1981@users.noreply.github.com>
2021-09-16 16:33:34 -0300

    Update QueriesExecution.java

--- src/main/java/part3/QueriesExecution.java
+++ src/main/java/part3/QueriesExecution.java
@@ -7,26 +7,31 @@ public class QueriesExecution {
 
     public static void main(String[] args){
         AlunoDAO alunoDAO = new AlunoDAO();
-        List<Aluno> alunos = alunoDAO.list();
-        alunos.stream().forEach(System.out::println);
-
-        Aluno alunoParaConsulta = alunoDAO.getById(1);
-       // System.out.println(alunoParaConsulta);
+        
         Aluno alunoParaInsercao = new Aluno(
                "Matheus",
                43,
              "SP"
         );
 
-        //alunoDAO.create(alunoParaInsercao);
-
-        alunoDAO.delete(1);
-
-        Aluno alunoParaAtualizar = alunoDAO.getById(3);
+        alunoDAO.create(alunoParaInsercao);
+        
+        Aluno alunoParaConsulta = alunoDAO.getById(1);
+        System.out.println(alunoParaConsulta);
+        
+        List<Aluno> alunos = alunoDAO.list();
+        alunos.stream().forEach(System.out::println);
+     
+        Aluno alunoParaAtualizar = alunoDAO.getById(1);
         alunoParaAtualizar.setNome("Joaquim");
         alunoParaAtualizar.setIdade(18);
         alunoParaAtualizar.setEstado("RS");
 
         alunoDAO.update(alunoParaAtualizar);
+        
+        Aluno alunoParaConsulta = alunoDAO.getById(1);
+        System.out.println(alunoParaConsulta);
+        
+        alunoDAO.delete(1);
     }
 }
